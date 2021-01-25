5560e28ec16ee17cc26bf10ffcaf5826b25e49a1
Steeve <steeve.boulanger.becode@gmail.com>
2021-01-25 11:57:19 +0100

    intro 1.4 js

---
+++ 1.4.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let firstName = readlineSync.question('Can you give me your first name please?');
+let name = readlineSync.question('Can you give me your name please?');
+let city = readlineSync.question('Can you give me your city please?');
+
+console.log("Hello my name is " + firstName, name + " and I live in " + city);
