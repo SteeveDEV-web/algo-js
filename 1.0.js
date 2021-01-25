74b6de90b4994aaed0630f3b21c36ef493f83ac2
Steeve <steeve.boulanger.becode@gmail.com>
2021-01-25 10:43:00 +0100

    intro 1.0 js

---
+++ 1.0.js
@@ -0,0 +1,12 @@
+// console.log (200 + 600); // Addition //
+
+// console.log (800 - 950); // Soustraction //
+
+// console.log (5480 * 540); // Multiplication //
+
+// console.log (2952200 / 450); // Division // 
+
+console.log (8 % 3);
+
+
+
